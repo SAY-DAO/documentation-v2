6f66f4e717357e14075ba8be9b486e0057153e8d
G <ehsan@say.company>
2025-11-30 19:10:09 +0330

    GA added

---
+++ packages/nextra/src/client/components/google-tag.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { GoogleTagManager } from '@next/third-parties/google'
+
+export const GoogleTag = () => {
+    return (
+        <>
+            <GoogleTagManager gtmId="GTM-5BGKS7RV" />
+        </>
+    );
+};
